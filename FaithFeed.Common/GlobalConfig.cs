b680836dbd15a70f9fefde144ef7beee3bd653ee
Ethan Boudreaux <ethanbdx@gmail.com>
2020-06-20 13:34:47 -0500

    Update database directory

--- FaithFeed.Common/GlobalConfig.cs
+++ FaithFeed.Common/GlobalConfig.cs
@@ -10,6 +10,6 @@ namespace FaithFeed.Common
 {
     public static class GlobalConfig
     {
-        public static string CnnString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory()}\FaithFeedSeed.mdf;Integrated Security=True";
+        public static string CnnString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory()}\Database\FaithFeedSeed.mdf;Integrated Security=True";
     }  
 }
